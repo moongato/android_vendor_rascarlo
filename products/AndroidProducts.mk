f947b69b9e2dbb75725fc53c66ba2123f414615d
rascarlo <rascarlo@gmail.com>
2013-08-08 17:00:17 +0200

    add grouper
    Conflicts:
    	products/AndroidProducts.mk

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,3 +1,6 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/rascarlo_emulator.mk \
-    $(LOCAL_DIR)/rascarlo_mako.mk
+    $(LOCAL_DIR)/rascarlo_mako.mk \
+    $(LOCAL_DIR)/rascarlo_grouper.mk
+    
+    
