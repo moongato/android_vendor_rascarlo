8cd40c9e85e22cc094e11af7d17cc44563d29955
moongato <moongato@ubuntu>
2014-01-13 20:48:06 -0800

    remove moto-x bionic files

--- products/moto-x_patch.mk
+++ products/moto-x_patch.mk
@@ -9,8 +9,6 @@ PRODUCT_COPY_FILES += \
 
 # Lib
 PRODUCT_COPY_FILES += \
-        $(LOCAL_PATH)/lib/libc.so:system/lib/libc.so \
         $(LOCAL_PATH)/lib/libcutils.so:system/lib/libcutils.so \
         $(LOCAL_PATH)/lib/libdvm.so:system/lib/libdvm.so \
-        $(LOCAL_PATH)/lib/libm.so:system/lib/libm.so \
         $(LOCAL_PATH)/lib/libqc-opt.so:system/lib/libqc-opt.so
