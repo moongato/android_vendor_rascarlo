92ba8c53841d4cfdd5efd04cc8f60bebda62333f
moongato <moongato@ubuntu>
2014-11-18 10:44:08 -0800

    try mako

---
+++ products/rascarlo_mako.mk
@@ -0,0 +1,21 @@
+# Inherit AOSP device configuration for mako.
+$(call inherit-product, device/lge/mako/full_mako.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/rascarlo/products/common.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := rascarlo_mako
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := mako
+PRODUCT_MODEL := Nexus 4
+PRODUCT_MANUFACTURER := LGE
+
+# Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=occam \
+    BUILD_FINGERPRINT=google/occam/mako:4.4.4/KTU84P/1227136:user/release-keys \
+    PRIVATE_BUILD_DESC="occam-user 4.4.4 KTU84P 1227136 release-keys"
+
+# Inherit media effect blobs
+-include vendor/rascarlo/products/common_media_effects.mk
